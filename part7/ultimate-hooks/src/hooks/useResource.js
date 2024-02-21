846d211ca57cbec32c8bf99b707ea005e530b3d0
Shaiharyaar <shaiharyaar.ahmad@onestream.live>
2024-02-21 23:29:36 +0200

    part 7.8 complete

---
+++ part7/ultimate-hooks/src/hooks/useResource.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+
+const useResource = (baseUrl) => {
+  let token = null;
+  const [resources, setResources] = useState([]);
+
+  const getAll = useCallback(async () => {
+    const response = await axios.get(baseUrl);
+    setResources(response.data);
+  }, [baseUrl]);
+
+  const create = async (newObject) => {
+    const config = {
+      headers: { Authorization: token },
+    };
+    const response = await axios.post(baseUrl, newObject, config);
+    setResources(resources.concat(response.data));
+  };
+
+  useEffect(() => {
+    getAll();
+  }, [getAll]);
+
+  const service = {
+    getAll,
+    create,
+  };
+
+  return [resources, service];
+};
+
+export default useResource;
